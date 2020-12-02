d7203b0a98ef304adb7ba33dd9bfd124451f2925
Geomty <69478549+Geomty@users.noreply.github.com>
2020-12-02 16:02:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const {app, BrowserWindow } = require('electron');
-
-
+const { app, BrowserWindow } = require('electron');
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -9,21 +7,14 @@ function createWindow() {
             nodeIntegration: true
         }
     });
-
     win.loadFile('./index.html');
 }
-
-
 app.whenReady().then(createWindow);
-
-
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
-
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
